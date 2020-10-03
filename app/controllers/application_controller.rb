ec2dcd5ea9eb369e6422de7f0603431613e29f27
Raveen K Sharma <raveen08@gmail.com>
2020-10-03 22:48:56 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,52 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    erb :index
   end
+
+   get '/articles/new' do #loads new form
+
+    erb :new
+   end
+
+   post '/articles' do  #creates a article
+    @article = Article.create(params)
+  end 
+
+  get '/articles' do #loads index page
+    @articles = Article.all
+    erb :index
+  end
+
+  get '/articles/:id' do  #loads show page
+    @article = Article.find_by_id(params[:id])
+    erb :show
+  end
+
+  get '/articles/:id/edit' do #loads edit form
+    @article = Article.find_by_id(params[:id])
+    erb :edit
+  end
+
+  post '/articles/:id' do  #updates a article
+    @article = Article.find_by_id(params[:id])
+    @article.name = params[:title]
+    @article.content = params[:content]
+    @article.save
+  end
+
+  get '/articles/:id/delete' do 
+    erb :delete
+  end
+
+
+
+  post '/articles/:id/delete' do #delete action
+    @article = Article.find_by_id(params[:id])
+    @article.delete
+    redirect to '/articles'
+  end
+    
+  
+  
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,12 @@
 #Placeholder for a model
 
 class Article < ActiveRecord::Base
+  attr_accessor :title, :content
+  
+  def initalize(params)
+    @title = params[:title]
+    @content = params[:content]
+  end  
+  
+  
 end

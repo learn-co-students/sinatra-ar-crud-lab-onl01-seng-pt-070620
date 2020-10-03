0d8d717fc18ff7cce108bf7a798add434c6403fe
Raveen K Sharma <raveen08@gmail.com>
2020-10-03 23:30:49 +0000

    Done.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,12 +1,7 @@
 #Placeholder for a model
 
 class Article < ActiveRecord::Base
-  attr_accessor :title, :content
-  
-  def initalize(params)
-    @title = params[:title]
-    @content = params[:content]
-  end  
+
   
   
 end

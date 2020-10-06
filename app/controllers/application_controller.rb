270b3b8e932b42989a955f3f27f03e2530a5e2c4
kinnari <kinnari1022@yahoo.com>
2020-10-06 13:32:40 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,20 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  #create new form to create a new article
+  get "/articles/new" do
+    erb :new
   end
+
+  #create(post)
+  #redirects to index, to veiw all articles
+  post '/articles' do
+    @article = Article.create(params)
+    redirect to "/articles/#{@article.id}"
+  end
+
+    get '/articles' do
+      binding.pry
+    end
+
 end

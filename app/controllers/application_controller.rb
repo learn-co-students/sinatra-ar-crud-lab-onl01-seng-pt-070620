be0e27f72a2b49f00927194d32d489c6737c75c5
JakeKViti <thevitij64@gmail.com>
2020-10-08 02:01:13 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,46 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    redirect to "/articles"
   end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  get '/articles/new' do
+    @articles = Article.new
+    erb :new
+  end
+   
+  post '/articles' do
+    @articles = Article.create(params)
+    redirect to "/articles/#{@articles.id}"
+  end
+
+  get '/articles/:id' do
+    @articles = Article.find_by_id(params[:id])
+    erb :show
+  end
+
+  get '/articles/:id/edit' do  #load edit form
+    @articles = Article.find_by_id(params[:id])
+    erb :edit
+  end
+
+  patch '/articles/:id' do #edit action
+    @articles = Article.find_by_id(params[:id])
+    @articles.update(params[:article])
+    redirect to "/articles/#{@articles.id}"
+  end
+  
+  delete '/articles/:id' do #delete action
+    @articles = Article.find_by_id(params[:id])
+    @articles.delete
+    redirect to '/articles'
+  end
+  
+
+
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 #Placeholder for a model
 
-class Article < ActiveRecord::Base
+class Article < ActiveRecord::Base  
+    validates_presence_of :title, :content
 end
